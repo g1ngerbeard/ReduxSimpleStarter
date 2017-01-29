576ba28d1a2dd88da5dd324301f40eaf0f32c041
gingerbeard <kulishovt@gmail.com>
2017-01-29 01:39:42 +0100

    part 2 lection 33

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,19 +1,42 @@
 import React, {Component} from "react";
 
+//const SearchBar = ({onTermInput}) => {
+//    return (
+//        <div className="search-bar">
+//            <input
+//                onKeyPress={e => e.key === 'Enter' ? onTermInput(e.target.value) : null}
+//                onBlur={e => onTermInput(e.target.value)}
+//            />
+//        </div>
+//    );
+//};
+
+
 class SearchBar extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {term: 'type something...'}
+        this.state = {
+            term: ''
+        }
     }
 
     render() {
         return (
-            <div>
-                <input value={this.state.term} onChange={event => this.setState({term: event.target.value})}/>
+            <div className="search-bar">
+                <input
+                    value={this.state.term}
+                    onChange={e => this.updateState(e.target.value)}
+                />
             </div>
         );
     }
+
+    updateState(term) {
+        this.setState({term});
+        this.props.onTermInput(term)
+    }
+
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,46 @@
+import _ from 'lodash'
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
+import VideoDetails from "./components/video_detail"
 const API_KEY = 'AIzaSyBLN-BoCpR80Np-e1UHhht83CtF0QoFVis';
 
 class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {videos: []};
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
 
-        YTSearch({key: API_KEY, term: 'kitties'}, videos => {
-            this.setState({videos})
+        this.searchVideos('planking')
+    }
+
+    searchVideos(term) {
+        YTSearch({key: API_KEY, term: term}, videos => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            })
         });
     }
 
     render() {
+
+        const onTermInput = _.debounce((term)=> {
+            this.searchVideos(term)
+        }, 500);
+
         return (
             <div>
-                <SearchBar />
-                <VideoList videos={this.state.videos}/>
+                <SearchBar onTermInput={onTermInput}/>
+                <VideoDetails video={this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos}/>
             </div>
         );
     }
