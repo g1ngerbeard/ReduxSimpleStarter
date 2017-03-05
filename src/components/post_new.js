1671822574663f13187b9a5109f7ede7ab4b9993
gingerbeard <kulishovt@gmail.com>
2017-03-06 00:57:44 +0100

    part 6 lection 83

---
+++ src/components/post_new.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+class PostNew extends Component {
+    render() {
+        return (
+            <div>New post</div>
+        );
+    }
+}
+
+export default PostNew;
