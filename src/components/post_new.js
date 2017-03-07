751874f2057dc668442f8e7a0d54155ba0cb9f91
gingerbeard <kulishovt@gmail.com>
2017-03-08 00:10:21 +0100

    part 6 lection 87

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,12 +1,36 @@
 import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import {reduxForm} from 'redux-form';
 
 class PostNew extends Component {
     render() {
+        const {fields:{title, categories, content}, handleSubmit} = this.props;
+
         return (
-            <div>New post</div>
+            <form onsubmit={handleSubmit}>
+                <h3>Add new post</h3>
+
+                <div className="form-group">
+                    <label>Title</label>
+                    <input type="text" className="form-control" {...title}/>
+                </div>
+
+                <div className="form-group">
+                    <label>Categories</label>
+                    <input type="text" className="form-control" {...categories}/>
+                </div>
+
+                <div className="form-group">
+                    <label>Content</label>
+                    <input type="text-area" className="form-control" {...content}/>
+                </div>
+
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
         );
     }
 }
 
-export default PostNew;
+export default reduxForm({
+    form: 'PostNewForm',
+    fields: ['title', 'categories', 'content']
+})(PostNew);
--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {fetchPosts} from '../actions/index';
+import {Link} from 'react-router'
 
 class PostsIndex extends Component {
     componentWillMount() {
@@ -9,7 +10,13 @@ class PostsIndex extends Component {
 
     render() {
         return (
-            <div>My blog posts</div>
+            <div>
+                <div className="text-xs-right">
+                    <Link to="/posts/new" className="btn btn-primary">
+                        New post
+                    </Link>
+                </div>
+            </div>
         );
     }
 }
